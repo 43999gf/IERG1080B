6a372d5ca1ea61c52554b10fe507ed47a60a8903
43999gf <a1648857741@gmail.com>
2026-01-08 14:10:23 +0800

    Update lab01ex02.py

--- lab01ex02.py
+++ lab01ex02.py
@@ -22,8 +22,9 @@
 def printCourse(title = "Python", dept = "IERG", code = 1080, gpa_max = 4.0):
     
     # Replace [Input Your Format String] with your answer. You may edit the arguments.
-    print("The course {1} {2} is about '{0}'.\nYou can get at most {3} GPA.".format(title, dept, code, gpa_max))
+    print("The course {1} {2} is about '{0}'.\nYou can get at most {3:.2f} GPA.".format(title, dept, code, gpa_max))
 
 
 if __name__ == "__main__":
+
     printCourse() 
